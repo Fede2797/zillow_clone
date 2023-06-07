1e00eb5b0b0338dc0bd245710ecebeaf6fb7c27c
Federico Passarelli <federico2797@gmail.com>
2023-06-07 13:04:35 -0300

    Finished sidebar

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,31 @@
 import { Navbar } from './Navbar';
 import { SearchBar } from './SearchBar';
+import { useState } from "react"
 
 export const Header = () => {
+
+  const [showsideBar, setShowSideBar] = useState(true);
+  const toggleSideBar = () => setShowSideBar( !showsideBar );
+
   return (
     <header className='relative w-full h-[308px] bg-[url("pictures/blue-house-mobile.webp")] bg-cover bg-no-repeat bg-center sm:h-[488px] sm:bg-[url("pictures/blue-house-sm.webp")]'>
-        <Navbar />
+        <Navbar showsideBar={showsideBar} toggleSideBar={toggleSideBar}/>
         <div className='absolute w-full h-full p-4 left-0 top-0 flex flex-col items-center justify-center'>
           <h1 className='mb-[15px] text-[26px] text-center text-white font-frank-ruhl drop-shadow-[0px_0px_5px_#000] md:text-[36px]'>Find it. Tour it. Own it.</h1>
           <SearchBar />
+        </div>
+        {/* SideBar */}
+        <div className={`${ showsideBar ? 'block' : 'hidden'} absolute top-0 bottom-0 left-0 right-0 h-screen bg-white z-auto`}>
+          <ul className='mt-[60px] w-full flex flex-col font-open-sans text-[15px] [&>*]:py-3 [&>*]:pl-[13px] [&>*]:h-[53px] [&>*]:border-t-[1px] [&>*:last-child]:border-b-[1px]'>
+            <li>Buy</li>
+            <li>Rent</li>
+            <li>Sell</li>
+            <li>Home Loans</li>
+            <li>Agent finder</li>
+            <li>Manage Rentals</li>
+            <li>Advertise</li>
+            <li>Help</li>
+          </ul>
         </div>
     </header>
   )
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,12 +1,22 @@
 
-export const Navbar = () => {
+type NavbarProps = {
+  showsideBar: boolean;
+  toggleSideBar: () => void;
+};
+
+export const Navbar: React.FC<NavbarProps> = ({showsideBar, toggleSideBar}) => {
+
   return (
     <div className="relative w-full h-[60px] py-[18px] px-4 font-open-sans z-20 lg:h-[80px] lg:bg-white">
       <div className="max-w-[1280px] mx-auto h-full flex justify-between">
         {/* Left menu */}
         <div className="flex items-center [&>*]:hover:cursor-pointer [&>*]:z-20">
-          <button className="lg:hidden">
-              <img className="w-[18px] h-[18px]" src="icons/hamburger.svg" alt="" />
+          <button 
+            className="lg:hidden"
+            onClick={ toggleSideBar }
+          >
+              <img className={`${ showsideBar ? 'hidden' : 'block'} w-[18px] h-[18px]`} src="icons/hamburger.svg" alt="" />
+              <img className={`${ showsideBar ? 'block' : 'hidden'} ml-[-3px] w-[26px] h-[26px]`} src="icons/close.svg" alt="" />
           </button>
           <ul className="hidden lg:flex gap-6">
             <li>Buy</li>
@@ -21,7 +31,7 @@ export const Navbar = () => {
           <li className="hidden lg:block">Manage Rentals</li>
           <li className="hidden lg:block">Advertise</li>
           <li className="hidden lg:block">Help</li>
-          <li>
+          <li className={`${ showsideBar ? 'hidden' : 'block'}`}>
             <a href="#" className="font-open-sans text-[15px] text-white lg:text-black">
                 Sign in
             </a>
@@ -30,7 +40,8 @@ export const Navbar = () => {
       </div>
       {/* Zillow logo/home */}
       <a className="absolute left-0 top-0 h-[60px] w-full flex items-center lg:h-[80px]">
-          <img className="h-[25px] mx-auto lg:hidden hover:cursor-pointer z-20" src="logos/z-logo-white.svg" alt="" />
+          <img className={`${ showsideBar ? 'hidden' : 'block'} h-[25px] mx-auto lg:hidden hover:cursor-pointer z-20`} src="logos/z-logo-white.svg" alt="" />
+          <img className={`${ showsideBar ? 'block' : 'hidden'} h-[25px] m-auto lg:hidden hover:cursor-pointer z-20`} src="logos/z-logo-default.svg" alt="" />
           <img className="h-[35px] m-auto lg:pl-8 hidden lg:block hover:cursor-pointer z-20" src="logos/z-logo-default.svg" alt="" />
       </a>
     </div>
