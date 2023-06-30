0f0f98dd1aad03ca626f6bf0aed2009c642d5f15
Fede2797 <91157539+Fede2797@users.noreply.github.com>
2023-06-30 09:16:19 -0300

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
         <div className="absolute top-0 left-0 w-full h-[308px] bg-smhero bg-cover bg-no-repeat sm:h-[488px] 1xs:bg-mdhero md:bg-lghero lg:bg-xlhero"></div>
         <Navbar showsideBar={showsideBar} toggleSideBar={toggleSideBar}/>
         <div className='absolute w-full h-full p-4 left-0 top-0 flex flex-col items-center justify-center'>
-          <h1 className='mb-[15px] text-[26px] text-center text-white font-frank-ruhl drop-shadow-[0px_0px_5px_#000] md:text-[36px]'>Agents2. Tours. Loans. Homes.</h1>
+          <h1 className='mb-[15px] text-[26px] text-center text-white font-frank-ruhl drop-shadow-[0px_0px_5px_#000] md:text-[36px]'>Agents. Tours. Loans. Homes.</h1>
           <SearchBar />
         </div>
         {/* SideBar */}
